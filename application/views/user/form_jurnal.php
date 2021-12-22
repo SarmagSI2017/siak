1ff124fe8e9fb29386b46ba0f665374601784b05
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-12-22 08:05:53 +0700

    Update form_jurnal.php

--- application/views/user/form_jurnal.php
+++ application/views/user/form_jurnal.php
@@ -31,14 +31,14 @@
     <!-- Header -->
     <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
       <div class="container-fluid">
-        <div class="header-body">    
+        <div class="header-body">
         </div>
       </div>
     </div>
     <!-- Page content -->
     <div class="container-fluid mt--7">
       <div class="row">
-        <div class="col-xl-8 mb-5 mb-xl-0">       
+        <div class="col-xl-8 mb-5 mb-xl-0">
         </div>
       </div>
       <div class="row mt-5">
@@ -47,56 +47,101 @@
             <div class="card-header border-0">
               <div class="row align-items-center">
                 <div class="col">
-                  <h3 class="mb-3"><?= $title ?> Jurnal Umum</h3>
+                  <h3 class="mb-3"><?= $title ?> Rekam Transaksi</h3>
                 </div>
                 <div class="col-12 my-3 form-1">
                   <form action="<?= base_url($action) ?>" method="post">
-                  <?php 
-                    if(!empty($id)):
-                  ?>
-                  <input type="hidden" name="id" value="<?= $id ?>">
-                  <?php endif; ?>
-                  <div class="row mb-4">
-                    <div class="col-4">
-                        <label for="datepicker">Tanggal</label>
-                        <div class="input-group input-group-alternative">
+                    <div class="control-group after-add-more">
+                      <?php
+                      if (!empty($id)) :
+                      ?>
+                        <input type="hidden" name="id" value="<?= $id ?>">
+                      <?php endif; ?>
+                      <div class="row mb-4">
+                        <div class="col-4">
+                          <label for="datepicker">Tanggal</label>
+                          <div class="input-group input-group-alternative">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
+                              <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
                             </div>
                             <input class="form-control" id="datepicker" name="tgl_transaksi" type="text" value="<?= $data->tgl_transaksi ?>">
+                          </div>
+                          <?= form_error('tgl_transaksi') ?>
                         </div>
-                        <?= form_error('tgl_transaksi') ?>
+                      </div>
+                      <div class="row mb-4">
+                        <div class="col">
+                          <label for="no_reff">Nama Akun</label>
+                          <?= form_dropdown('no_reff', getDropdownList('akun', ['no_reff', 'nama_reff']), $data->no_reff, ['class' => 'form-control', 'id' => 'no_reff']); ?>
+                          <?= form_error('no_reff') ?>
+                        </div>
+                        <div class="col">
+                          <label for="reff">No. Reff</label>
+                          <input type="text" name="reff" class="form-control" id="reff" readonly>
+                        </div>
+                        <div class="col">
+                          <label for="jenis_saldo">Jenis Saldo</label>
+                          <?= form_dropdown('jenis_saldo', ['debit' => 'Debit', 'kredit' => 'Kredit'], $data->jenis_saldo, ['class' => 'form-control jenis_saldo', 'id' => 'jenis_saldo']); ?>
+                          <?= form_error('jenis_saldo') ?>
+                        </div>
+                        <div class="col">
+                          <label for="saldo">Saldo</label>
+                          <input type="text" name="saldo" class="form-control saldo" id="saldo" value="<?= $data->saldo ?>">
+                          <?= form_error('saldo') ?>
+                        </div>
+                        <div class="col">
+                          <label for="addon" class="mb-5"> </label>
+                          <button class="btn btn-success mt-3 add-more" type="button">
+                            <i class="glyphicon glyphicon-plus"></i> Add
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div class="row mb-4">
-                    <div class="col">
+                    <hr>
+                    <div class="col-12" id="form_jurnal_prepend">
+                      <button class="btn btn-primary" type="submit" id="button_jurnal"><?= $title ?></button>
+                    </div>
+                  </form>
+                </div>
+
+                <div class="copy invisible">
+                  <div class="control-group">
+                    <?php
+                    if (!empty($id)) :
+                    ?>
+                      <input type="hidden" name="id" value="<?= $id ?>">
+                    <?php endif; ?>
+                    <div class="row mb-4">
+                      <div class="col">
                         <label for="no_reff">Nama Akun</label>
-                        <?=form_dropdown('no_reff',getDropdownList('akun',['no_reff','nama_reff']),$data->no_reff,['class'=>'form-control','id'=>'no_reff']);?>
+                        <?= form_dropdown('no_reff', getDropdownList('akun', ['no_reff', 'nama_reff']), $data->no_reff, ['class' => 'form-control', 'id' => 'no_reff']); ?>
                         <?= form_error('no_reff') ?>
-                    </div>
-                    <div class="col">
+                      </div>
+                      <div class="col">
                         <label for="reff">No. Reff</label>
                         <input type="text" name="reff" class="form-control" id="reff" readonly>
-                    </div>
-                    <div class="col">
+                      </div>
+                      <div class="col">
                         <label for="jenis_saldo">Jenis Saldo</label>
-                        <?=form_dropdown('jenis_saldo',['debit'=>'Debit','kredit'=>'Kredit'],$data->jenis_saldo,['class'=>'form-control jenis_saldo','id'=>'jenis_saldo']);?>
+                        <?= form_dropdown('jenis_saldo', ['debit' => 'Debit', 'kredit' => 'Kredit'], $data->jenis_saldo, ['class' => 'form-control jenis_saldo', 'id' => 'jenis_saldo']); ?>
                         <?= form_error('jenis_saldo') ?>
-                    </div>
-                    <div class="col">
+                      </div>
+                      <div class="col">
                         <label for="saldo">Saldo</label>
                         <input type="text" name="saldo" class="form-control saldo" id="saldo" value="<?= $data->saldo ?>">
                         <?= form_error('saldo') ?>
+                      </div>
+                      <div class="col">
+                        <label for="addon" class="mb-5"> </label>
+                        <button class="btn btn-danger mt-3 remove" type="button"><i class="glyphicon glyphicon-remove"></i> Remove</button>
+                      </div>
                     </div>
+                    <hr>
                   </div>
-                  <div class="col-12" id="form_jurnal_prepend">
-                    <button class="btn btn-primary" type="submit" id="button_jurnal"><?= $title ?></button>
-                  </div> 
-                  </form> 
+
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      
