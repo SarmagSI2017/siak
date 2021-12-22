4a35408e39fe434a350bea47eeb3e59f9c682cdd
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-12-22 08:05:57 +0700

    Update footer.php

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,66 +1,79 @@
 <!-- Footer -->
 <footer class="footer">
-        <div class="row align-items-center justify-content-xl-between">
-          <div class="col-xl-6">
-            <div class="copyright text-center text-xl-left text-muted">
-              &copy; 2021 <a class="font-weight-bold ml-1" target="_blank">SARMAG SIB</a>
-            </div>
-          </div>
-        </div>
-      </footer>
+  <div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-6">
+      <div class="copyright text-center text-xl-left text-muted">
+        &copy; 2021 <a class="font-weight-bold ml-1" target="_blank">SARMAG SIB</a>
+      </div>
     </div>
   </div>
-  <!-- Argon Scripts -->
-  <!-- Core -->
-  <script src="<?= base_url('assets/vendor/jquery/dist/jquery.min.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
-  <!-- Optional JS -->
-  <script src="<?= base_url('assets/vendor/chart.js/dist/Chart.min.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/chart.js/dist/Chart.extension.js') ?>"></script>
-  <!-- Argon JS -->
-  <script src="<?= base_url('assets/js/argon.js?v=1.0.0') ?>"></script>
-  <!-- Datepicker -->
-  <script src="<?= base_url('assets/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') ?>"></script>
-  <!-- SWEETALERT -->
-  <script src="<?= base_url('assets/vendor/sweetalert/sweetalert2.all.min.js') ?>"></script>
-  <!-- Custom JS -->
-  <script src="<?= base_url('assets/js/script.js') ?>"></script>
-  <?php
-    $pesan = $this->session->flashdata('berhasil');
-    if(!empty($pesan)):
-  ?>
-    <!-- SCRIPT SWEETALERT INLINE -->
-    <script>
-      $(window).on('load',function(){
-        let pesan = "<?= $pesan ?>";
-        swal('Berhasil!',pesan,'success');
-      });
-    </script>
-  <?php endif; ?>
+</footer>
+</div>
+</div>
+<!-- Argon Scripts -->
+<!-- Core -->
+<script src="<?= base_url('assets/vendor/jquery/dist/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
+<!-- Optional JS -->
+<script src="<?= base_url('assets/vendor/chart.js/dist/Chart.min.js') ?>"></script>
+<script src="<?= base_url('assets/vendor/chart.js/dist/Chart.extension.js') ?>"></script>
+<!-- Argon JS -->
+<script src="<?= base_url('assets/js/argon.js?v=1.0.0') ?>"></script>
+<!-- Datepicker -->
+<script src="<?= base_url('assets/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') ?>"></script>
+<!-- SWEETALERT -->
+<script src="<?= base_url('assets/vendor/sweetalert/sweetalert2.all.min.js') ?>"></script>
+<!-- Custom JS -->
+<script src="<?= base_url('assets/js/script.js') ?>"></script>
+<script>
+  $(document).ready(function() {
+    $(".add-more").click(function() {
+      var html = $(".copy").html();
+      $(".after-add-more").after(html);
+    });
 
-  <?php
-    $pesan = $this->session->flashdata('berhasilHapus');
-    if(!empty($pesan)):
-  ?>
-    <script>
-      $(window).on('load',function(){
-        let pesan = "<?= $pesan ?>";
-        swal('Berhasil!',pesan,'success');
-      });
-    </script>
-  <?php endif; ?>  
+    // saat tombol remove dklik control group akan dihapus 
+    $("body").on("click", ".remove", function() {
+      $(this).parents(".control-group").remove();
+    });
+  });
+</script>
+<?php
+$pesan = $this->session->flashdata('berhasil');
+if (!empty($pesan)) :
+?>
+  <!-- SCRIPT SWEETALERT INLINE -->
+  <script>
+    $(window).on('load', function() {
+      let pesan = "<?= $pesan ?>";
+      swal('Berhasil!', pesan, 'success');
+    });
+  </script>
+<?php endif; ?>
 
-  <?php
-    $pesan = $this->session->flashdata('dataNull');
-    if(!empty($pesan)):
-  ?>
-    <script>
-      $(window).on('load',function(){
-        let pesan = "<?= $pesan ?>";
-        swal('Oops!',pesan,'error');
-      });
-    </script>
-  <?php endif; ?>  
+<?php
+$pesan = $this->session->flashdata('berhasilHapus');
+if (!empty($pesan)) :
+?>
+  <script>
+    $(window).on('load', function() {
+      let pesan = "<?= $pesan ?>";
+      swal('Berhasil!', pesan, 'success');
+    });
+  </script>
+<?php endif; ?>
+
+<?php
+$pesan = $this->session->flashdata('dataNull');
+if (!empty($pesan)) :
+?>
+  <script>
+    $(window).on('load', function() {
+      let pesan = "<?= $pesan ?>";
+      swal('Oops!', pesan, 'error');
+    });
+  </script>
+<?php endif; ?>
 </body>
 
 </html>
