4553e3a93e192cd0bc6f70c51c51fa2a44f78aac
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2022-01-11 17:28:10 +0700

    Update form_akun.php

--- application/views/user/form_akun.php
+++ application/views/user/form_akun.php
@@ -17,17 +17,40 @@
                   </div>
                   <div class="col-12 my-3">
                     <form action="<?= base_url($action) ?>" method="post">
+                      <div class="row">
+                        <div class="col-sm-12">
+                          <div class="form-group form-group-default">
+                            <label><h4><b>Sub Header Akun</b></h4></label>
+                            <select class="form-control" name="sub_header" id="sub_header" onchange="addData()" required="required">
+                              <option disabled selected>-- Daftar Sub Header --</option>
+                              <option value="<?= $data->no_reff ?>" ><?= $data->nama_reff ?></option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
                       <div class="form-group">
-                        <label for="no_ref">No. Reff</label>
+                        <label for="no_ref"><h4><b>No. Reff</b></h4></label>
                         <p><?= form_error('no_reff') ?></p>
                         <input type="text" name="no_reff" class="form-control mb-3" id="no_reff" value="<?= $data->no_reff ?>" <?php if ($title == 'Edit') {
                                                                                                                                   echo "readonly";
                                                                                                                                 } ?>>
                       </div>
                       <div class="form-group">
-                        <label for="nama">Nama Reff</label>
+                        <label for="nama"><h4><b>Nama Akun</b></h4></label>
                         <p><?= form_error('nama_reff') ?></p>
                         <input type="text" name="nama_reff" class="form-control mb-3" id="nama" value="<?= $data->nama_reff ?>">
+                      </div>
+                      <div class="row">
+                        <div class="col-sm-12">
+                          <div class="form-group form-group-default">
+                            <label><h4><b>Saldo Normal</b></h4></label>
+                            <select class="form-control" name="saldo_normal" id="saldo_normal" required="required">
+                              <option disabled selected>-- Pilih Saldo Normal --</option>
+                              <option value="Debit" >Debit</option>
+                              <option value="Kredit" >Kredit</option>
+                            </select>
+                          </div>
+                        </div>
                       </div>
                       <div class="form-group">
                         <label for="keterangan">Keterangan</label>
