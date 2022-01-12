677172534a81ff9b495ef727e002f9468641648d
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2022-01-12 16:30:25 +0700

    Update data_akun_new.php

--- application/views/user/dataakun/data_akun_new.php
+++ application/views/user/dataakun/data_akun_new.php
@@ -17,10 +17,17 @@
         <div class="card-header">
           <div class="row align-items-center py-0">
             <div class="col-lg-6 col-7">
-              <h6 class="h2 d-inline-block mb-0">Data Akun </h6>
+              <h6 class="h2 d-inline-block mb-0">Unsur Data Akun </h6>
+              <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4 text-primary">
+              <ol class="breadcrumb breadcrumb-links">
+                <li class="breadcrumb-item text-black"><a href="#"><i class="fas fa-home text-black"></i></a></li>
+                <li class="breadcrumb-item active text-black" aria-current="page">Unsur Data Akun</li>
+              </ol>
+            </nav>
             </div>
             <div class="col-6 text-right">
               <a href="<?= base_url('data_akun/all') ?>" class="btn btn-primary mt-2">Lihat List Akun</a>
+              <a href="<?= base_url('data_akun/tambah')?>" class="btn btn-primary mt-2">Tambah Akun</a>
             </div>
           </div>
         </div>
