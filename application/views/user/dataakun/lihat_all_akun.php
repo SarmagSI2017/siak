4d68b8a66b45d7112959c53ba0aed81d0cb13578
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2022-01-12 16:30:19 +0700

    Update lihat_all_akun.php

--- application/views/user/dataakun/lihat_all_akun.php
+++ application/views/user/dataakun/lihat_all_akun.php
@@ -18,19 +18,28 @@
           <div class="row align-items-center py-0">
             <div class="col-lg-6 col-7">
               <h6 class="h2 d-inline-block mb-0">List Data Akun </h6>
+              <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4 text-primary">
+                <ol class="breadcrumb breadcrumb-links">
+                  <li class="breadcrumb-item text-black"><a href="#"><i class="fas fa-home text-black"></i></a></li>
+                  <li class="breadcrumb-item text-black"><a href="<?= base_url('data_akun') ?>">Data Unsur Akun</a></li>
+                  <li class="breadcrumb-item active text-black" aria-current="page">Lihat Semua Data Akun</li>
+                </ol>
+              </nav>
             </div>
             <div class="col-6 text-right">
-              <a href="<?= base_url('data_akun') ?>" class="btn btn-primary mt-2">Lihat Unsur Akun</a>
+              <a href="<?= base_url('data_akun') ?>" class="btn btn-primary mt-2">Lihat Lihat Semua Akun</a>
+              <a href="<?= base_url('data_akun/tambah') ?>" class="btn btn-primary mt-2">Tambah Akun</a>
             </div>
           </div>
         </div>
         <div class="table-responsive py-4">
           <table class="table table-flush" id="datatable-basic">
-          <thead class="thead-light">
+            <thead class="thead-light">
               <tr>
                 <th>No. Reff</th>
                 <th class="text-center">Nama Akun</th>
                 <th class="text-center">Saldo Normal</th>
+                <th class="text-center">Keterangan</th>
                 <th class="text-center">Aksi</th>
               </tr>
             </thead>
@@ -49,9 +58,12 @@
                   <td>
                     <?= $row->saldo_normal ?>
                   </td>
+                  <td>
+                    <?= $row->keterangan ?>
+                  </td>
                   <td class="d-flex justify-content-center">
-                    <a href="<?= base_url() ?>" class="btn btn-primary mb-4">Detail</a>
-                    <a href="<?= base_url('data_akun/edit/'.$row->no_reff) ?>" class="btn btn-warning mb-4">Edit</a> 
+
+                    <a href="<?= base_url('data_akun/edit/' . $row->no_reff) ?>" class="btn btn-warning mb-4">Edit</a>
                   </td>
                 </tr>
               <?php endforeach; ?>
@@ -61,4 +73,3 @@
       </div>
     </div>
   </div>
-
