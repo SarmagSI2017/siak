c8919b604e7a72b933916cb8368464274ba6b403
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2022-01-12 17:22:16 +0700

    Update laporan_aset_netto_detail.php

--- application/views/user/laporan_aset_netto_detail.php
+++ application/views/user/laporan_aset_netto_detail.php
@@ -3,6 +3,13 @@
         <div class="container-fluid">
             <div class="header-body">
                 <div class="row align-items-center py-4">
+                      <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4 text-primary">
+                        <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                          <li class="breadcrumb-item text-black"><a href="<?= base_url('dashboard') ?>"><i class="fas fa-home text-black"></i></a></li>
+                          <li class="breadcrumb-item text-black"><a href="<?= base_url('dashboard') ?>">Laporan Aset Netto</a></li>
+                          <li class="breadcrumb-item active text-black" aria-current="page">Detail Laporan Aset Netto</li>
+                        </ol>
+                      </nav>
                 </div>
             </div>
         </div>
