932927ebdb66dee53e0602afc510e4d52f434f59
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2022-01-12 16:30:08 +0700

    Update jurnal_umum.php

--- application/views/user/jurnal_umum.php
+++ application/views/user/jurnal_umum.php
@@ -19,7 +19,14 @@
             <div class="card-header border-0">
               <div class="row align-items-center">
                 <div class="col">
-                  <h3 class="mb-0">Rekam Transaksi</h3>
+                <h6 class="h2 d-inline-block mb-0">Detail Rekam Transaksi </h6>
+                  <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4 text-primary">
+                    <ol class="breadcrumb breadcrumb-links">
+                      <li class="breadcrumb-item text-black"><a href="#"><i class="fas fa-home text-black"></i></a></li>
+                      <li class="breadcrumb-item active text-black" aria-current="page" href="<?= base_url('jurnal_umum') ?>">Rekam Transaksi</li>
+                      <li class="breadcrumb-item active text-black" aria-current="page">Detail Rekam Transaksi</li>
+                    </ol>
+                  </nav>
                 </div>
               </div>
             </div>
